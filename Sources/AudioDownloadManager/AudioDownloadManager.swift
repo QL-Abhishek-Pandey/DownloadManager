57c939d604a4a4d985bc41031000ac87aae465e7
Abhishek Pandey <abhishek.pandey@quokkalabs.com>
2023-08-29 16:56:22 +0530

    Audio Downloader

--- Sources/AudioDownloadManager/AudioDownloadManager.swift
+++ Sources/AudioDownloadManager/AudioDownloadManager.swift
@@ -1,6 +1,13 @@
-public struct AudioDownloadManager {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public class AudioDownloader {
+    public static func downloadAudio(from url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data {
+                completion(.success(data))
+            }
+        }.resume()
     }
 }
