aced5866b36c0086356040ceff57ccd10d2dae85
Abhishek Pandey <abhishek.pandey@quokkalabs.com>
2023-08-29 21:41:06 +0530

    [REFACTOR]: Change the file of manager

--- Sources/AudioDownloadManager/AudioDownloadManager.swift
+++ Sources/AudioDownloadManager/AudioDownloadManager.swift
@@ -1,42 +1,21 @@
+
 import Foundation
+import Combine
 
-public class AudioDownloadManager: NSObject {
-    
-    var session:URLSession?
-    var dataTask:URLSessionDownloadTask?
-    typealias progressClosure = ((TaskResult) -> Void)
-    var downloadAudioCallback: progressClosure!
-    
-    public func downloadAudio(with url: String) {
-        if let audioUrl = URL(string: url) {
-            let configuration = URLSessionConfiguration.default
-            let manqueue = OperationQueue.main
-            session = Foundation.URLSession(configuration: configuration, delegate:self, delegateQueue: manqueue)
-            dataTask = session?.downloadTask(with: audioUrl)
-            dataTask?.resume()
-        } else {
-            downloadAudioCallback(.failure("InValid URL"))
+@available(macOS 10.15, *)
+public class AudioDownloadManager: ObservableObject {
+    private var downloadManager = AudioDownloadTask()
+    private var cancellables = Set<AnyCancellable>()
+
+    @Published var taskResult: TaskResult = .progress(0.0)
+
+    init() {
+        downloadManager.downloadAudioCallback = { [weak self] result in
+            self?.taskResult = result
         }
     }
 
-}
-
-extension AudioDownloadManager: URLSessionDownloadDelegate {
-   public func urlSession(_ session: URLSession,
-                    downloadTask: URLSessionDownloadTask,
-                    didWriteData bytesWritten: Int64,
-                    totalBytesWritten: Int64,
-                    totalBytesExpectedToWrite: Int64) {
-        let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
-        if progress < 1 && progress > 0.0 {
-        downloadAudioCallback(.progress(progress))
-        }
-    }
-    
- public func urlSession(_ session: URLSession,
-                    downloadTask: URLSessionDownloadTask,
-                    didFinishDownloadingTo location: URL) {
-        downloadAudioCallback(.downloaded(location))
-      
+    func downloadAudio(with url: String) {
+        downloadManager.downloadAudio(with: url)
     }
 }
---
+++ Sources/AudioDownloadManager/AudioDownloadTask.swift
@@ -0,0 +1,49 @@
+//
+//  AudioDownloadTask.swift
+//  
+//
+//  Created by Abhishek Pandey on 29/08/23.
+//
+
+import Foundation
+
+public class AudioDownloadTask: NSObject {
+    
+    var session:URLSession?
+    var dataTask:URLSessionDownloadTask?
+    typealias progressClosure = ((TaskResult) -> Void)
+    var downloadAudioCallback: progressClosure!
+    
+    public func downloadAudio(with url: String) {
+        if let audioUrl = URL(string: url) {
+            let configuration = URLSessionConfiguration.default
+            let manqueue = OperationQueue.main
+            session = Foundation.URLSession(configuration: configuration, delegate:self, delegateQueue: manqueue)
+            dataTask = session?.downloadTask(with: audioUrl)
+            dataTask?.resume()
+        } else {
+            downloadAudioCallback(.failure("InValid URL"))
+        }
+    }
+
+}
+
+extension AudioDownloadTask: URLSessionDownloadDelegate {
+   public func urlSession(_ session: URLSession,
+                    downloadTask: URLSessionDownloadTask,
+                    didWriteData bytesWritten: Int64,
+                    totalBytesWritten: Int64,
+                    totalBytesExpectedToWrite: Int64) {
+        let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+        if progress < 1 && progress > 0.0 {
+        downloadAudioCallback(.progress(progress))
+        }
+    }
+    
+ public func urlSession(_ session: URLSession,
+                    downloadTask: URLSessionDownloadTask,
+                    didFinishDownloadingTo location: URL) {
+        downloadAudioCallback(.downloaded(location))
+      
+    }
+}
