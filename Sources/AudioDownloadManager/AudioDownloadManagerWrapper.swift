e867c43e65e236e7909448b76c482a0665c32239
Abhishek Pandey <abhishek.pandey@quokkalabs.com>
2023-08-29 21:57:35 +0530

    AudioDownloadManagerWrapper

---
+++ Sources/AudioDownloadManager/AudioDownloadManagerWrapper.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Abhishek Pandey on 29/08/23.
+//
+
+import Foundation
+import Combine
+
+@available(macOS 10.15, *)
+public class AudioDownloadManagerWrapper: ObservableObject {
+    
+}
+
