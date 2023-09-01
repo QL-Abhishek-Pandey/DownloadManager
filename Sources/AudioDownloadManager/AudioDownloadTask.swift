42eadc6ef505143981a3406dc9b35ce34ec471d4
Abhishek Pandey <abhishek.pandey@quokkalabs.com>
2023-09-01 13:54:26 +0530

    [REFACTOR]

--- Sources/AudioDownloadManager/AudioDownloadTask.swift
+++ Sources/AudioDownloadManager/AudioDownloadTask.swift
@@ -21,9 +21,9 @@ public class AudioDownloadTask: NSObject {
     public func downloadMedia(with url: String) {
         audioURl = url
         if let audioUrl = URL(string: audioURl) {
-            let configuration = URLSessionConfiguration.default//background(withIdentifier: "com.cleanclock.ios")
-            let manqueue = OperationQueue.main
-            session = Foundation.URLSession(configuration: configuration, delegate:self, delegateQueue: manqueue)
+            let configuration = URLSessionConfiguration.background(withIdentifier: "com.cleanclock.ios")
+           // let manqueue = OperationQueue.main
+            session = URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
             dataTask = session?.downloadTask(with: audioUrl)
             dataTask?.resume()
         } else {
